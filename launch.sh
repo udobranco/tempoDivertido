43ce06b8003816b8f49df16c9cfa7b5dac101467
udobranco <udobranco@gmail.com>
2015-09-29 09:38:42 +0100

    working directory fix

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-docker run -dt -v /home/udobranco/test:/opt/app -it devops-node python /opt/app/pySocketCli.py 0
-docker run -dt -v /home/udobranco/test:/opt/app -it devops-node python /opt/app/pySocketCli2.py 1
-docker run -dt -v /home/udobranco/test:/opt/app -it devops-node python /opt/app/pySocketCli.py 2
-docker run -dt -v /home/udobranco/test:/opt/app -it devops-node python /opt/app/pySocketCli2.py 3
-docker run -dt -v /home/udobranco/test:/opt/app -it devops-node python /opt/app/pySocketCli.py 4
+docker run -dt -v /home/udobranco/tempodivertido:/opt/app -it devops-node python /opt/app/pySocketCli.py 0
+docker run -dt -v /home/udobranco/tempodivertido:/opt/app -it devops-node python /opt/app/pySocketCli2.py 1
+docker run -dt -v /home/udobranco/tempodivertido:/opt/app -it devops-node python /opt/app/pySocketCli.py 2
+docker run -dt -v /home/udobranco/tempodivertido:/opt/app -it devops-node python /opt/app/pySocketCli2.py 3
+docker run -dt -v /home/udobranco/tempodivertido:/opt/app -it devops-node python /opt/app/pySocketCli.py 4
 
