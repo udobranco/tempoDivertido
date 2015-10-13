b061fc58a697b1b3ac7919724e9118a5aa4c9db0
udobranco <udobranco@gmail.com>
2015-10-13 17:24:08 +0000

    sender update for port listening

--- rabbit/sender.js
+++ rabbit/sender.js
@@ -1,7 +1,7 @@
-var qSocket = "amqp://localhost";
+var qSocket = "amqp://172.17.0.20:55555";
 var qName = "nodejs.demo";
 var encoding = "utf8";
-var message = proces.argv.splice(2).join(" ") || "n/a";
+var message = process.argv.splice(2).join(" ") || "n/a";
 
 
 var context = require ("rabbit.js").createContext(qSocket);
@@ -14,7 +14,7 @@ context.on("ready", function(){
         pub.connect(qName, function(){
                 console.log("[x] Connected");
 		
-		var data = JSON.stringify({message:message})M
+		var data = JSON.stringify({message:message})
 		pub.write(data, encoding);
 		console.log ("[x] sent mesage: %s", message)
 	
