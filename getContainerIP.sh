8d18c541be266a954ded6c5ccc023ae9e273cada
udobranco <udobranco@gmail.com>
2015-11-21 17:29:37 +0000

    usage: docker-ip YOUR_CONTAINER_ID

--- getContainerIP.sh
+++ getContainerIP.sh
@@ -1,4 +1,5 @@
 #Add to bashrc (~/.bashrc) this lines and reload it (source ~/.bashrc)
+#usage docker-ip YOUR_CONTAINER_ID
 docker-ip() {
  docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
 }
