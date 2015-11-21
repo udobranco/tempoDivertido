ad08338cc62fd9dada54780c2a3fac9605e37bba
udobranco <udobranco@gmail.com>
2015-11-21 19:11:55 +0000

    prepare db cluster. cp file to /home/

--- db-cluster/getRPM.sh
+++ db-cluster/getRPM.sh
@@ -1,5 +1,19 @@
+
+echo "install Wget tool"
+yum install wget -y 
+
 echo "fetching shared gpl"
 wget http://ftp.kaist.ac.kr/mysql/Downloads/MySQL-Cluster-7.4/MySQL-Cluster-shared-gpl-7.4.7-1.el7.x86_64.rpm
 
 echo "fetching server gpl"
 wget http://ftp.kaist.ac.kr/mysql/Downloads/MySQL-Cluster-7.4/MySQL-Cluster-server-gpl-7.4.7-1.el7.x86_64.rpm
+
+
+echo "install cluster shared"
+yum install MySQL-Cluster-shared-gpl-7.4.7-1.el7.x86_64.rpm -y
+
+echo "install perl-data-dumper"
+yum install perl-Data-Dumper -y
+
+echo "install shared gpl"
+yum install MySQL-Cluster-server-gpl-7.4.7-1.el7.x86_64.rpm -y
