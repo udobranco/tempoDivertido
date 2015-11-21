418c98f0a0e9970676bec085168f3124d5267840
udobranco <udobranco@gmail.com>
2015-11-21 20:57:26 +0000

    firewall config

--- db-cluster/management_node/settingMN.sh
+++ db-cluster/management_node/settingMN.sh
@@ -3,3 +3,8 @@ cp ./config.ini /var/lib/mycluster
 
 echo "starting service"
 ndb_mgmd -f /var/lib/mycluster/config.ini
+
+echo "firewall config"
+firewall-cmd --permanent --add-port=1186/tcp
+firewall-cmd reload
+
