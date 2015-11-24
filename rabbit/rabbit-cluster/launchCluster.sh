4566d95dfbd83538a8c6a7b6e6d7552e8edf7359
udobranco <udobranco@gmail.com>
2015-11-24 18:20:31 +0000

    Update launchCluster.sh

--- rabbit/rabbit-cluster/launchCluster.sh
+++ rabbit/rabbit-cluster/launchCluster.sh
@@ -5,3 +5,4 @@ echo ">>>>> launching cluster 2/3 <<<<<"
 docker run -dt -it -h rab02 --name rab02 -p 10.5.50.20:22222:22222 rabbitmq:3 /bin/bash
 echo ">>>>> launching cluster 3/3 <<<<<"
 docker run -dt -it -h rab03 --name rab03 -p 10.5.50.20:33333:33333 rabbitmq:3 /bin/bash
+docker ps -a
